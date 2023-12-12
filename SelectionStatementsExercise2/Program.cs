ba6c06127954f094a9b707b2ef3eda16641e011b
Kim Sedlacek <sedlaceks@optonline.net>
2023-12-12 12:45:12 -0500

    selection statements ex 2 completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,32 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your favorite school subject?");
+            Console.WriteLine("What is your favorite subject in school? (please type in lower case)");
+            string favSubj = Console.ReadLine();
 
-            var subject = Console.ReadLine();
-            switch (subject)
-            {
-                case "Math": Console.WriteLine("Math is a fun subject, I like it too.");
-                    break;
-
-                case "PE": Console.WriteLine("I love PE, we all need to get out of the classroom and move around.");
-                    break;
-
-                case "English": Console.WriteLine("English is an essential subject, both Reading and Writing" +
-                    "are very important, I'm glad you like it.");
-                    break;
-
-                case "Science": Console.WriteLine("Science is so much fun, I love all the experiments!");
-                    break;
-
-                case "Social Studies": Console.WriteLine("Great, Social Studies is always changing and I like that" +
-                    "about it.");
-                    break;
-                default: Console.WriteLine($"Oh yes, {subject} is a wonderful subject, I'm glad you're enjoying it!");
-                   break;
-
-            }
             
+            switch (favSubj)
+            {
+                case "art":
+                    Console.WriteLine("Art is so much fun, I love having a creative outlet at school.");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is very important, I'm glad you like it!");
+                    break;
+
+                case "pe":
+                    Console.WriteLine("I love PE too, I'm glad we get a chance to run around and move our bodies.");
+                    break;
+
+                case "history":
+                    Console.WriteLine("That's great that history is your favorite subject, it's important to know where we came from.");
+                    break;
+
+                case "english":
+                    Console.WriteLine("English is difficult for many people; you must be a good writer and enjoy reading.");
+                    break;
+
+                case "math":
+                    Console.WriteLine("Math is fun and challenging!");
+                    break;
+
+                default:
+                    Console.WriteLine($"Yes, {favSubj} is great subject to study, I'm glad it's your favorite!");
+                    break;
+            }
+
+
+
+
         }
+
+        
     }
 }
